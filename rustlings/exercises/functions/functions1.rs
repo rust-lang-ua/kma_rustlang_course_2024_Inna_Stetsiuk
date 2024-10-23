81e9d03094b18dad2c0d683840c188c7153600c6
Inez-git <innastetsiuk@gmail.com>
2024-10-23 17:52:46 +0300

    functions module done

--- rustlings/exercises/functions/functions1.rs
+++ rustlings/exercises/functions/functions1.rs
@@ -1,8 +1,13 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
+
+fn call_me() {
+    println!("function call me")
+}
 
 fn main() {
     call_me();
 }
+
